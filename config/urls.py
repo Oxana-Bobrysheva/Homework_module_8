eb4079e71d356042bef150eaf5fa28f13e17d8b2
Oxana Bobrysheva <bobrysheva_oxana@mail.ru>
2025-08-25 18:29:34 +0300

    ViewSet for users added

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("study/", include("study.urls", namespace="study"))
+    path("study/", include("study.urls", namespace="study")),
+    path("users/", include("users.urls", namespace="users")),
 ]
