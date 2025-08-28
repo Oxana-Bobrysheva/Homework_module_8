ba5d383153f866788947ea6f2fd9cb4f3b2b7222
Oxana Bobrysheva <bobrysheva_oxana@mail.ru>
2025-08-28 13:47:30 +0300

    task 1 in homework 30_2

--- study/serializers.py
+++ study/serializers.py
@@ -1,16 +1,22 @@
+from django.core.serializers import serialize
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from study.models import Course, Lesson
 
 
 class CourseSerializer(ModelSerializer):
+    lessons_count=serializers.SerializerMethodField()
     class Meta:
         model = Course
         fields = (
             "course_name",
             "preview",
             "description",
+            "lessons_count",
         )
+    def get_lessons_count(self, obj):
+        return obj.lessons.count()
 
 
 class LessonSerializer(ModelSerializer):
