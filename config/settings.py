22e592a3d238f3ad516afcd50b9222fcf3748e4d
Oxana Bobrysheva <bobrysheva_oxana@mail.ru>
2025-10-14 21:34:05 +0300

    database settings for ci.yml changed

--- config/settings.py
+++ config/settings.py
@@ -90,13 +90,9 @@ DATABASES = {
 }
 if 'test' in sys.argv:
     DATABASES["default"] = {
-            "ENGINE": "django.db.backends.postgresql_psycopg2",
-            "NAME": os.getenv("NAME_TESTS"),
-            "USER": os.getenv("POSTGRES_USER"),
-            "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-            "HOST": os.getenv("HOST", "db" if os.getenv("DATABASE_URL") else "localhost"),
-            "PORT": os.getenv("PORT", "5432"),
-        }
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    }
 
 
 CACHES = {
